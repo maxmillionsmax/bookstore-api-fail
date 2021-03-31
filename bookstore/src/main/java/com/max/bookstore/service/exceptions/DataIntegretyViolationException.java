7fc43a997b068a837ccbb76a0604fed35e2f3dcf
maxmilianocaetano <maxmilianocaetano@gmail.com>
2021-03-31 13:22:42 -0300

    Excecao personalizada para DELETE de categoria

---
+++ bookstore/src/main/java/com/max/bookstore/service/exceptions/DataIntegretyViolationException.java
@@ -0,0 +1,17 @@
+package com.max.bookstore.service.exceptions;
+
+public class DataIntegretyViolationException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public DataIntegretyViolationException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DataIntegretyViolationException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
